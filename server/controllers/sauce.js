65d9ea937f887092f4fa07ec27f6a5d64dcc6bc1
Zansuken <doom.sebastien.pro@gmail.com>
2022-11-11 17:33:48 +0100

    (fix): fixed like controller

--- server/controllers/sauce.js
+++ server/controllers/sauce.js
@@ -11,6 +11,10 @@ exports.createSauce = (req, res) => {
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
     }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
   });
 
   try {
@@ -78,21 +82,80 @@ exports.getSauces = (req, res) => {
     .catch((error) => res.status(400).json({ error }));
 };
 
-// TODO: Fix the like/dislike controller
 exports.updateSauceLikes = (req, res) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
-      if (sauce.userId !== req.auth.userId) {
-        res.status(401).json({ message: "Not authorized." });
-      } else {
-        const updatedSauce = { ...sauce, like: req.body.like };
-        Sauce.updateOne(
-          { _id: req.params.id },
-          { ...updatedSauce, _id: req.params.id }
-        )
-          .then(() => res.status(200).json({ message: "Item updated." }))
-          .catch((error) => res.status(401).json({ error }));
+      const { likes, dislikes, usersLiked, usersDisliked } = sauce;
+      const userId = req.body.userId;
+      const action = req.body.like;
+
+      const didUserLiked = usersLiked.includes(userId);
+      const didUserDisliked = usersDisliked.includes(userId);
+
+      let updateParams = {};
+
+      switch (action) {
+        case 0:
+          if (didUserLiked && likes !== 0) {
+            updateParams = {
+              $inc: { likes: -1 },
+              $pull: { usersLiked: userId },
+            };
+          }
+          if (didUserDisliked && dislikes !== 0) {
+            updateParams = {
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: userId },
+            };
+          }
+          break;
+
+        case 1:
+          if (didUserDisliked) {
+            updateParams = {
+              $inc: { likes: 1 },
+              $inc: { dislikes: -1 },
+              $push: { usersLiked: userId },
+              $pull: { usersDisliked: userId },
+            };
+          } else {
+            updateParams = {
+              $inc: { likes: 1 },
+              $push: { usersLiked: userId },
+            };
+          }
+          break;
+
+        case -1:
+          if (didUserLiked) {
+            updateParams = {
+              $inc: { likes: -1 },
+              $inc: { dislikes: 1 },
+              $pull: { usersLiked: userId },
+              $push: { usersDisliked: userId },
+            };
+          } else {
+            updateParams = {
+              $inc: { dislikes: 1 },
+              $push: { usersDisliked: userId },
+            };
+          }
+          break;
+
+        default:
+          break;
       }
+
+      Sauce.findOneAndUpdate({ _id: req.params.id }, updateParams)
+        .then(() =>
+          res.status(200).json({
+            message: "Item updated.",
+          })
+        )
+        .catch((error) => {
+          console.error(error);
+          res.status(401).json({ error });
+        });
     })
     .catch((error) => res.status(400).json({ error }));
 };
